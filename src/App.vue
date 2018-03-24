f6721c633e771e2f1efc9c5739845e611c08bd1c
Dani Ene <daene17@gmail.com>
2018-03-23 17:45:33 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/dx_header_logo.png">
+    <h1>DxOrders</h1>
+    <img src="./assets/jumbotron.jpg">
+    <p> We are a healthcare app dedicated to helping bring together doctors in patients in order to make it easier for
+    users to keep track of their health needs</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+body{
+  background-color: #C0C0C0;
+}
+
+
+
+</style>
